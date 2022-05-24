8e9e1edc13ccd20d70af2702f6c19a18f96d899f
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-05-24 12:48:46 -0500

    uso de view binding en la actividad principal

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.window.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModel
-
+import com.example.practiceapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val viewModel = MainViewModel().islouding
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         installSplashScreen().apply {
-            setKeepOnScreenCondition{viewModel.value}
+            setKeepOnScreenCondition { viewModel.value }
         }
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
 }

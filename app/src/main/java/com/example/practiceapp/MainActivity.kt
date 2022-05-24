201751ceaabdbef8a181f4e6feb89f5872131193
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-05-24 01:12:52 -0500

    se agrego splash con delay de tres segundos

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.practiceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val viewModel = MainViewModel().islouding
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{viewModel.value}
+        }
         setContentView(R.layout.activity_main)
-
     }
-
 
 }
